2f8bcc1bb099b2f231ffd4099a92178a81c1cde2
Gustavo Cruz <gu.cruz17@hotmail.com>
2020-07-08 15:08:34 -0300

    Created project

---
+++ src/main/java/com/gustavo/gitpalm/GitpalmApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.gitpalm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpalmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpalmApplication.class, args);
+	}
+
+}
